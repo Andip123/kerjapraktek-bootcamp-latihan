a4e26e2dfa85aa24ef83f0f150d52909adce4eef
andip <andiprabandaru@gmail.com>
2024-03-22 02:38:35 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// let nama = 'Andi prabandaru'
+// let usia = 20
+// let tinggiBadan = 162.5
+// let beratBadan
+// let pacar = null 
+
+// beratBadan = 60
+
+// if (pacar == null) {
+//     pacar = 'belum punya'
+// } else {
+//     pacar = 'udah punya'
+// }
+//  let saldoAwal = 100000
+//  let saldoTambahan = 120000
+//  const hutang = 50000
+//  const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// alert(
+//     `nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan} kg dan pacar saya ${pacar}`,
+// )
+// alert(
+//     `saldo awal saya sebesar Rp.${saldoAwal} & saldo tambahan yang akan saya miliki sebesar Rp.${saldoTambahan} jadi total saldo yang saya miliki adalah sebanyak Rp.${saldoAkhir}`,
+// )
+
+
+
+// val = today
+// val = today.toString();
+// let hari = new Date(). getDay()
+// console.log(`hari ke ${hari}`,)
+
+
+const day = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+let today = new Date().getDay();
+const namaHari = day [today];
+console.log('Hari ini adalah: ' + namaHari);
+
+
